75f10e95544a1e8f602962f9229d7e3973a62ead
nschubi <dev@schulzebilk.de>
2024-04-15 16:01:01 +0200

    draw basic grid

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ function createSquareDiv(size){
 
 function createGrid(container, numSquares){
     // Check if the height or width of container is smaller
-    console.log("container.heigt", container.style.height)
-    console.log("container.width", container.style.width)
-    let size = container.height < container.width ? container.height / numSquares : container.width / numSquares;
+    let width = container.offsetWidth;
+    let height = container.offsetHeight;
+    console.log("container height", height);
+    console.log("container width", width);
+    let size = height < width ? height / numSquares : width / numSquares;
+    console.log(size);
     for(i = 0; i < numSquares; i++){
-        container.appendChild(createSquareDiv(100));
+        container.appendChild(createSquareDiv(200));
     }
 }
 

6441cdcad7c5d630480a21f5c62add0f78f117af
nschubi <dev@schulzebilk.de>
2024-04-18 16:47:18 +0200

    add basic draw functionality

--- script.js
+++ script.js
@@ -2,23 +2,51 @@ function createSquareDiv(size){
     const square = document.createElement("div");
     square.style.minHeight = size + 'px';
     square.style.minWidth = size + 'px';
-    square.style.borderColor = "black";
-    square.style.borderStyle = "solid";
+    // square.style.borderColor = "black";
+    // square.style.borderStyle = "solid";
+    // square.style.cursor="default";
     return square;
 }
 
-function createGrid(container, numSquares){
+function createGrid(container, rowSize){
     // Check if the height or width of container is smaller
+    container.innerHTML = '';
     let width = container.offsetWidth;
     let height = container.offsetHeight;
+    let numSquares = rowSize * rowSize;
     console.log("container height", height);
     console.log("container width", width);
-    let size = height < width ? height / numSquares : width / numSquares;
+    let area = width * height;
+    let areaTile = area / numSquares;
+    let size = Math.sqrt(areaTile);
     console.log(size);
     for(i = 0; i < numSquares; i++){
-        container.appendChild(createSquareDiv(200));
+        container.appendChild(createSquareDiv(size));
     }
 }
 
 const cont = document.querySelector("#container");
-createGrid(cont, 16);
+let rowSize = 24;
+let mousedown = false;  
+cont.addEventListener("mousedown", (e) => {
+   mousedown = true;
+   draw(e);
+});
+cont.addEventListener("mouseup", (e) => {
+    mousedown = false;
+ });
+cont.addEventListener("mouseover", (e) => {
+    draw(e);
+});
+
+function draw(e){
+    if(mousedown){
+        e.target.style.backgroundColor = "black";
+    }
+}
+
+const btnReset = document.querySelector("#btnReset");
+console.log(btnReset);
+btnReset.addEventListener("click", createGrid(cont, rowSize));
+
+createGrid(cont, rowSize);
